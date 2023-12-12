fbbc1c26190570798db1c4eae089781a17c0bded
kangmin <lnewgatel@gmail.com>
2023-12-12 10:09:16 +0900

    ResponseEntity, findbyPhoneNumber

--- src/main/java/com/example/greenpage/repository/StudentRepository.java
+++ src/main/java/com/example/greenpage/repository/StudentRepository.java
@@ -6,7 +6,10 @@ import com.example.greenpage.model.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+    Optional<Student> findByPhoneNumber(String phoneNumber);
 }
